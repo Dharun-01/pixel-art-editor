dcbed650b9bb95c17ee770d38bdacab35c2cd5f2
Dharun-01 <dharun01.dev@gmail.com>
2026-03-19 01:58:52 +0530

    Fixed another deployment bug

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [react()],
+	base: '/pixel-art-editor/',
 	build: {
 		target: 'esnext',
-		base: '/pixel-art-editor/',
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, 'index.html'),
